b72b1113ca178a8d34f24b53da977d716db28968
mattscicluna <mattcscicluna@gmail.com>
2017-11-26 11:23:08 -0500

    trained model again and pushed

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd char-rnn.pytorch/
 python train.py data/all_tex_files.txt --cuda
-python generate.py all_tex_files.pt --cuda
+python generate.py all_tex_files.pt --cuda -t 0.5 -l 200 --prime_str "It is trivial to see that: "
