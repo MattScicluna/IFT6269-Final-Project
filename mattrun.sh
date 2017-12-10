69e524c7800ec8f5c1dc033310acd29b69bb5c85
mattscicluna <mattcscicluna@gmail.com>
2017-12-10 12:34:29 -0500

    added new model with 1k weights 3 layers

--- mattrun.sh
+++ mattrun.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python char-rnn.pytorch/train.py --train_set data/train.txt --valid_set data/valid.txt --model lstm --cuda
+python char-rnn.pytorch/train.py --train_set data/train.txt --valid_set data/valid.txt --model lstm --cuda --hidden_size 1000 --batch_size 300
