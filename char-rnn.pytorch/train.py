638ed09ac77013a9ab289bb216c5a7c5ea4aa04f
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2017-12-09 22:51:29 -0500

    stop training when validation loss start increasing

--- char-rnn.pytorch/train.py
+++ char-rnn.pytorch/train.py
@@ -4,6 +4,7 @@
 import argparse
 import os
 import random
+import sys
 import time
 
 # thirdparty imports
@@ -11,123 +12,125 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 
 # local imports
 from dataset import WordDataset, n_characters, time_since
-from generate import generate
+from evaluate import evaluate
 from helpers import read_file
 from helpers import char_tensor
 from helpers import prep_data
 from model import CharRNN
 
-# Parse command line arguments
-argparser = argparse.ArgumentParser()
-argparser.add_argument('filename', type=str)
-argparser.add_argument('--model', type=str, default="gru")
-argparser.add_argument('--n_epochs', type=int, default=30)
-argparser.add_argument('--print_every', type=int, default=2)
-argparser.add_argument('--hidden_size', type=int, default=200)
-argparser.add_argument('--n_layers', type=int, default=3)
-argparser.add_argument('--learning_rate', type=float, default=0.01)
-argparser.add_argument('--chunk_len', type=int, default=200)
-argparser.add_argument('--batch_size', type=int, default=300)
-argparser.add_argument('--num_workers', type=int, default=8)
-argparser.add_argument('--cuda', action='store_true')
-argparser.add_argument('--cpu', action='store_true')
-argparser.add_argument('--full_dataset', action='store_true')
-argparser.add_argument('--model_file', type=str)
-args = argparser.parse_args()
-
-def random_training_set(chunk_len, batch_size):
-    inp = torch.LongTensor(batch_size, chunk_len)
-    target = torch.LongTensor(batch_size, chunk_len)
-    for bi in range(batch_size):
-        start_index = random.randint(0, file_len - chunk_len)
-        end_index = start_index + chunk_len + 1
-        chunk = file[start_index:end_index]
-        inp[bi] = char_tensor(chunk[:-1])
-        target[bi] = char_tensor(chunk[1:])
-    inp = Variable(inp)
-    target = Variable(target)
-    if args.cuda:
-        inp = inp.cuda()
-        target = target.cuda()
-    return inp, target
-
-def train(inp, target):
-    hidden = decoder.init_hidden(args.batch_size)
-    if args.cuda:
+def train(decoder, optimizer, criterion, inp, target, batch_size, chunk_len, cuda):
+    hidden = decoder.init_hidden(batch_size)
+    if cuda:
         hidden = hidden.cuda()
     decoder.zero_grad()
     loss = 0
 
-    for c in range(args.chunk_len):
+    for c in range(chunk_len):
         output, hidden = decoder(inp[:,c], hidden)
-        loss += criterion(output.view(args.batch_size, -1), target[:,c])
+        loss += criterion(output.view(batch_size, -1), target[:,c])
 
     loss.backward()
-    decoder_optimizer.step()
+    optimizer.step()
 
-    return loss.data[0] / args.chunk_len
+    return loss.data[0] / chunk_len
 
-def save():
-    if args.model_file:
-        filename = args.model_file
+def save(decoder, model_file, filename):
+    if model_file:
+        filename = model_file
     else:
-        filename = os.path.splitext(os.path.basename(args.filename))[0] + '.pt'
+        filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
     torch.save(decoder, filename)
     print('Saved as {}'.format(filename))
 
-# Initialize models and start training
+def main():
 
-decoder = CharRNN(
-    n_characters,
-    args.hidden_size,
-    n_characters,
-    model=args.model,
-    n_layers=args.n_layers,
-)
-decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr=args.learning_rate)
-criterion = nn.CrossEntropyLoss()
+    # Parse command line arguments
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--train_set', type=str, required=True)
+    argparser.add_argument('--valid_set', type=str, required=True)
+    argparser.add_argument('--model', type=str, default="gru")
+    argparser.add_argument('--n_epochs', type=int, default=30)
+    argparser.add_argument('--hidden_size', type=int, default=200)
+    argparser.add_argument('--n_layers', type=int, default=3)
+    argparser.add_argument('--learning_rate', type=float, default=0.01)
+    argparser.add_argument('--chunk_len', type=int, default=200)
+    argparser.add_argument('--batch_size', type=int, default=300)
+    argparser.add_argument('--num_workers', type=int, default=8)
+    argparser.add_argument('--cuda', action='store_true')
+    argparser.add_argument('--cpu', action='store_true')
+    argparser.add_argument('--model_file', type=str)
+    args = argparser.parse_args()
 
-if args.cuda:
-    decoder.cuda()
+    # Initialize models and start training
 
-start = time.time()
-loss_avg = 0
+    decoder = CharRNN(
+        n_characters,
+        args.hidden_size,
+        n_characters,
+        model=args.model,
+        n_layers=args.n_layers,
+    )
+    optimizer = torch.optim.Adam(decoder.parameters(), lr=args.learning_rate)
+    criterion = nn.CrossEntropyLoss()
 
-if args.full_dataset:
-    train_dataset = WordDataset(args.filename, args.chunk_len)
-    dataloader = DataLoader(train_dataset, batch_size=args.batch_size,
-                            shuffle=True, num_workers=args.num_workers,
-                            drop_last=True)
-else:
-    file, file_len = read_file(args.filename)
+    if args.cuda:
+        decoder.cuda()
 
-try:
-    print('Training for {} epochs...'.format(args.n_epochs))
-    for epoch in tqdm(range(1, args.n_epochs + 1)):
-        if args.full_dataset:
-            loss = 0
-            for sample in dataloader:
-                loss += train(*prep_data(sample['input'], sample['target'], args.cuda))
-                loss_avg += loss
-        else:
-            loss = train(*random_training_set(args.chunk_len, args.batch_size))
-            loss_avg += loss
+    start = time.time()
 
-        if epoch % args.print_every == 0:
+    train_dataset = WordDataset(args.train_set, args.chunk_len)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size,
+                                  shuffle=True, num_workers=args.num_workers,
+                                  drop_last=True)
+
+    valid_dataset = WordDataset(args.valid_set, args.chunk_len)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size,
+                                  shuffle=True, num_workers=args.num_workers,
+                                  drop_last=True)
+
+    try:
+        prev_valid_loss = sys.maxsize
+
+        print('Training for {} epochs...'.format(args.n_epochs))
+        for epoch in range(1, args.n_epochs + 1):
+
+            train_loss = 0
+            for s in train_dataloader:
+                input_, target = prep_data(s['input'], s['target'], args.cuda)
+                train_loss += train(decoder, optimizer, criterion, input_,
+                                    target, args.batch_size, args.chunk_len,
+                                    args.cuda)
+
+            valid_loss, num_samples = 0, 0
+            for s in valid_dataloader:
+                input_, target = prep_data(s['input'], s['target'], args.cuda)
+                valid_loss += evaluate(decoder, criterion, input_, target,
+                                       args.batch_size, args.chunk_len, args.cuda)
+                num_samples += 1
+            bpc = valid_loss / num_samples # bits per character
+
+            elapsed = time_since(start)
             pcnt = epoch / args.n_epochs * 100
-            log = '{} ({} {:.1f}%) {:.4f}'
-            print(log.format(time_since(start), epoch, pcnt, loss))
-            seed = 'It is trivial to see that '
-            print(generate(decoder, seed, 100, cuda=args.cuda), '\n')
+            log = ('{} elapsed - epoch #{} ({:.1f}%) - training loss {:.2f} '
+                   '- validation loss {:.2f} - BPC {:.2f}')
+            print(log.format(elapsed, epoch, pcnt, train_loss, valid_loss, bpc))
 
-    print("Saving...")
-    save()
+            if valid_loss > prev_valid_loss:
+                print('No longer learning, just overfitting, stopping here.')
+                break
 
-except KeyboardInterrupt:
-    print("Saving before quit...")
-    save()
+            prev_valid_loss = valid_loss
+
+        print("Saving...")
+        save(decoder, args.model_file, args.train_set)
+
+    except KeyboardInterrupt:
+        print("Saving before quit...")
+        save(decoder, args.model_file, args.train_set)
+
+if __name__ == '__main__':
+    main()
 
